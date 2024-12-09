45dd3f4095a8c87d2454cf62b1ed980543cc53a8
slay-a <srilayaponangi43@gmail.com>
2024-12-09 05:44:00 -0800

    api client changes

--- frontend/package/APIClient.js
+++ frontend/package/APIClient.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const APIClient = axios.create({
 //   baseURL: 'http://dhanutek-hrms.westus2.cloudapp.azure.com/api',
-  baseURL: 'http://localhost:3500',
+  baseURL: 'https://quizz-generator.onrender.com',
   // timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
